ab79a5b598cbf8c5320728e3b0a4ba109292900f
nataliakloc96-ui <natalia.kloc96@gmail.com>
2026-05-07 22:56:01 +0200

    Refactor database connection to use DATABASE_URL

--- backend/db.py
+++ backend/db.py
@@ -1,12 +1,11 @@
 import psycopg2
+import os
+
+DATABASE_URL = os.getenv("postgresql://scraper_1pbr_user:FgPpAiYSIygameqwN5e8zPi0X5vU4h4x@dpg-d7tr4nnavr4c73d0abrg-a.frankfurt-postgres.render.com/scraper_1pbr")
 
 def get_conn():
     
     return psycopg2.connect(
-        dbname="scraper_1pbr",
-        user="scraper_1pbr_user",
-        password="FgPpAiYSIygameqwN5e8zPi0X5vU4h4x",
-        host="dpg-d7tr4nnavr4c73d0abrg-a.frankfurt-postgres.render.com",
-        port="5432",
+        DATABASE_URL,        
         sslmode="require"
     )
