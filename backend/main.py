7cb139b5c09d859457215b8f6895ec7acbc8ed90
nataliakloc96-ui <natalia.kloc96@gmail.com>
2026-05-07 22:19:35 +0200

    Enhance API with CORS, Telegram notifications, and scheduler
    Added CORS middleware, Telegram integration, and auto-scraping functionality.

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,66 @@
 from fastapi import FastAPI 
 from scraper import scrape_jobs, save_jobs
 from db import get_conn
+from fastapi.middleware.cors import CORSMiddleware
+from apscheduler.schedulers.background import BackgroundScheduler
+from scraper import scrape_jobs, save_jobs
+import atexit
+import requests
+import os
+
+
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
+CHAT_ID = os.getenv("CHAT_ID")
+
+def send_telegram_message(message):
+    url = f"https://api.telegram.org/bot{8640982433:AAEleNJWJcfFAgL2M8rXh2BMWaCgvrHgUaU}/sendMessage"
+
+    requests.post(url, json={
+        "chat_id": CHAT_ID,
+        "text": message
+    })
+
+
+
+def init_db():
+    conn = get_conn()
+    cursor = conn.cursor()
+
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS jobs (
+                   id SERIAL PRIMARY KEY, 
+                   title TEXT,
+                   company TEXT,
+                   location TEXT
+        );
+
+    """)
+
+    conn.commit()
+    conn.close()
+
+init_db()
+
+def auto_scrape():
+    print("AUTO SCRAPE STARTED")
+    try:
+        jobs = scrape_jobs()
+        save_jobs(jobs)
+        print(f"Saved: {len(jobs)} jobs")
+    except Exception as e:
+        print("AUTO SCRAPE ERROR:", e)
+
 
 @app.get("/jobs")
 def get_jobs():
@@ -40,24 +98,34 @@ def run_scraper():
         print("ERROR:", e)
         return {"error": str(e)}
     
+
+
 @app.get("/debug-db")
 def debug_db():
     conn = get_conn()
     cursor = conn.cursor()
 
     cursor.execute (
-        "SELECT current_database();"
-    )
+        "SELECT current_database();")
     db = cursor.fetchone()
 
-    cursor.execute("""
-        SELECT table_name
-        FROM information_schema.tables
-        WHERE table_schema='public';
-    """)
-    tables = cursor.fetchall()
+    cursor.execute("SELECT inet_server_addr();")
+    host = cursor.fetchone()
+
+    cursor.execute("SELECT COUNT(*) FROM jobs;")
+    count = cursor.fetchone()
 
     return {
         "database": db,
-        "tables": tables
+        "host": host,
+        "count": count
     }
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("main:app", host="0.0.0.0", port=8000)
+
+scheduler = BackgroundScheduler()
+scheduler.add_job(auto_scrape, 'interval', minutes=10)
+scheduler.start()
+atexit.register(lambda: scheduler.shutdown())
