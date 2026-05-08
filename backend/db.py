c0a1238602514379c88374716b9e5181e3d64bdf
nataliakloc96-ui <natalia.kloc96@gmail.com>
2026-05-08 11:15:50 +0200

    Update DATABASE_URL to use environment variable

--- backend/db.py
+++ backend/db.py
@@ -1,8 +1,7 @@
 import psycopg2
 import os
 
-DATABASE_URL = os.getenv("postgresql://scraper_1pbr_user:FgPpAiYSIygameqwN5e8zPi0X5vU4h4x@dpg-d7tr4nnavr4c73d0abrg-a.frankfurt-postgres.render.com/scraper_1pbr")
-
+DATABASE_URL = os.getenv("DATABASE_URL")
 def get_conn():
     
     return psycopg2.connect(
