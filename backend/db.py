cc935772a1e09a67c003e1c8e46337a4bdc0b4d5
Natalia <6991nk@gmail.com>
2026-05-08 13:32:26 +0200

    Fix merge conflict in db.py

--- backend/db.py
+++ backend/db.py
@@ -1,21 +1,3 @@
-<<<<<<< HEAD
-import psycopg2
-import os
-
-
-def get_conn():
-    
-    return psycopg2.connect(
-        dbname="scraper_1pbr", 
-        user="scraper_1pbr_user", 
-        password="FgPpAiYSIygameqwN5e8zPi0X5vU4h4x", 
-        host="dpg-d7tr4nnavr4c73d0abrg-a.frankfurt-postgres.render.com", 
-        port="5432", 
-        sslmode="require"        
-        
-    )
-
-=======
 import psycopg2
 import os
 
@@ -26,4 +8,3 @@ def get_conn():
         DATABASE_URL,        
         sslmode="require"
     )
->>>>>>> d23084533f23a819d73ec70629333fa77bfec141
--- backend/scraper.py
+++ backend/scraper.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import requests
 from bs4 import BeautifulSoup
 from db import get_conn
@@ -64,66 +63,3 @@ if __name__ == "__main__":
     data = scrape_jobs()
     save_jobs(data)
     print("Zapisano:", len(data))
-=======
-import requests
-from bs4 import BeautifulSoup
-from db import get_conn
-import os
-from notifications import send_telegram
-
-seen_jobs = set()
-
-def scrape_jobs():
-    url = "https://realpython.github.io/fake-jobs/"
-    res = requests.get(url)
-
-    soup = BeautifulSoup(res.text, "html.parser")
-
-    jobs = []
-
-    for job in soup.select(".card-content"):
-        title = job.select_one("h2").text.strip()
-        company = job.select_one("h3").text.strip()
-        location = job.select_one(".location").text.strip()
-
-        jobs.append({
-            "title": title,
-            "company": company,
-            "location": location
-        })
-    
-    return jobs
-
-
-def save_jobs(jobs):
-    conn = get_conn()
-    cursor = conn.cursor()
-    print("CONNECTING TO DB...")
-    print("HOST:", os.getenv("DB_HOST"))
-
-    for job in jobs:
-        key = (job["title"], job["company"])
-
-        cursor.execute("""
-            INSERT INTO jobs (title, company, location) 
-            VALUES (%s, %s, %s)
-            ON CONFLICT (title, company, location) 
-            DO NOTHING
-        """, (title, company, location))
-             
-
-        
-
-        if key not in seen_jobs:
-            seen_jobs.add(key)
-
-            send_telegram(f"🆕 NEW JOB:\n{job['title']}\n{job['company']}\n{job['location']}"
-            )
-    conn.commit()
-    conn.close()
-
-if __name__ == "__main__":
-    data = scrape_jobs()
-    save_jobs(data)
-    print("Zapisano:", len(data))
->>>>>>> d23084533f23a819d73ec70629333fa77bfec141
